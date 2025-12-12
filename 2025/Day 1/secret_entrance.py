b9206bec49a03668dd3dc24be0812a718e5ddae7
cw500 <cw500@students.waikato.ac.nz>
2025-12-12 19:22:24 +1300

    Part 2 completed

--- 2025/Day 1/secret_entrance.py
+++ 2025/Day 1/secret_entrance.py
@@ -1,39 +1,43 @@
 
 # init variables
 min : int = 0
-max : int = 99
+max : int = 100
 position : int = 50
 count : int = 0
-num_turns: int = 0
 
-#file : str = "sample_input.txt"
+# input file
 file : str = "input.txt"
 
-# read file
+# read the file
 with open(file) as input:
+  # loop over the file
   for instruction in input:
+    # direction and val to turn the dial
     direction : str = instruction[0] 
     turn : int = int(instruction[1:])
 
-    # Turn in direction
-    if(direction == "L"):
-      position -= turn
-    
-    elif(direction == "R"):
-      position += turn
-    
-    # Calibrate if past boundaries
-    while(position > 99):
-      position -= 100
-    while(position < 0):
-      position += 100
+    # calc full rotations and the remainer
+    rotations, remainder = divmod(turn, max)
+    count += rotations
 
-    # Count num times we hit pos 0
-    if(position == 0):
-        count += 1
+    # make the remainder negative if turning left
+    remainder = -remainder if direction == "L" else remainder
 
-    num_turns += 1
-    #print(position)
+    # where the position will be (before transforming to fit the min and max)
+    next_position = position + remainder
 
+    # if position is on 0 (it has already been counted for)
+    if position != 0:
+        # if direction is left and is below min add 1 to count
+        if direction == "L" and next_position <= min:
+            count += 1
+        # if direction is right and is above max add 1 to count
+        elif direction == "R" and next_position >= max:
+            count += 1
+
+    # if next pos is negative it will make it positive with the correct amount of rotation
+    position = next_position % 100
+
+# print out count
 print(count)
-print(num_turns)
+
